cb1355a3e5816f780a29981e0512f29348092175
dkkim1111 <dkkim1111@naver.com>
2025-07-02 12:58:40 +0900

    fix: 30개 이상 미처리 문제 수정

--- src/main/app/modules/threads/threads.service.ts
+++ src/main/app/modules/threads/threads.service.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from '@main/app/modules/common/prisma/prisma.service'
 import { Injectable, Logger } from '@nestjs/common'
 import { shuffle } from 'lodash'
+import { createHash } from 'node:crypto'
 import { ElementHandle } from 'playwright'
 import { SettingsService } from '../settings/settings.service'
 import { WorkflowService } from '../workflow/workflow.service'
@@ -59,80 +60,83 @@ export class ThreadsService {
       this.logging(jobId, '게시물을 찾습니다')
       await this.workflowService.waitArticles(page, articleSelector)
 
-      let articles = await page.$$(articleSelector)
+      const processedArticles = new Set()
 
-      let currentArticleIndex = 0
-      let processedCount = 0
-      while (processedCount < maxCount) {
-        if (currentArticleIndex >= articles.length) {
-          articles = await page.$$(articleSelector)
-          currentArticleIndex = 0
-        }
-        const article = articles[processedCount]
-        if (!article) {
-          this.logging(jobId, '게시물을 찾지 못했습니다')
-          break
-        }
-        await page.evaluate(el => {
-          el.scrollIntoView({ behavior: 'smooth' })
-        }, article)
-        await page.waitForTimeout(1000)
+      while (processedArticles.size < maxCount) {
+        const articles = await page.$$(articleSelector)
+        for (const article of articles) {
+          const desc = await article.$('div > div:nth-child(3) > div > div:nth-child(1)')
+          const descText = await desc?.textContent()
+          let articlaHash = ''
+          try {
+            articlaHash = createHash('md5').update(descText).digest('hex')
+          } catch (err) {
+            // 간혹 오류 발생하는 경우있어서 skip
+            continue
+          }
 
-        currentArticleIndex++
-        processedCount++
-        const desc = await article.$('div > div:nth-child(3) > div > div:nth-child(1)')
-        const descText = await desc?.textContent()
-        const articleText = `[${descText?.slice(0, 20)}...]`
-        this.logging(jobId, `${articleText} 게시물을 처리합니다. (${processedCount} / ${maxCount})`)
+          if (processedArticles.has(articlaHash)) {
+            continue
+          }
+          await page.evaluate(el => {
+            el.scrollIntoView({ behavior: 'smooth' })
+          }, article)
+          await page.waitForTimeout(1000)
 
-        if (followAction) {
-          this.logging(jobId, `${articleText} 팔로우를 시도합니다`)
-          try {
-            await this.workflowService.followArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
-            this.logging(jobId, `${articleText} 팔로우 성공`)
-          } catch (err) {
-            this.logging(jobId, `${articleText} 팔로우 결과: ${err.message}`)
-          }
-          await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
-        }
-        if (likeAction) {
-          this.logging(jobId, `${articleText} 좋아요를 시도합니다`)
-          try {
-            await this.workflowService.likeArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
-            this.logging(jobId, `${articleText} 좋아요 성공`)
-          } catch (err) {
-            this.logging(jobId, `${articleText} 좋아요 결과: ${err.message}`)
-          }
-          await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
-        }
-        if (repostAction) {
-          try {
-            this.logging(jobId, `${articleText} 리포스트를 시도합니다`)
-            await this.workflowService.repostArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
-            this.logging(jobId, `${articleText} 리포스트 성공`)
-          } catch (err) {
-            this.logging(jobId, `${articleText} 리포스트 결과: ${err.message}`)
-          }
-          await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
-        }
-        if (commentAction) {
-          this.logging(jobId, `${articleText} 댓글을 시도합니다`)
-          const filteredFollowMessages = followMessages.filter(message => message.trim() !== '')
-          if (filteredFollowMessages.length === 0) {
-            this.logging(jobId, `멘트가 없어 댓글을 작성하지 않습니다.`)
-          } else {
+          const articleText = `[${descText?.slice(0, 20)}...]`
+          this.logging(jobId, `${articleText} 게시물을 처리합니다. (${processedArticles.size} / ${maxCount})`)
+
+          if (followAction) {
+            this.logging(jobId, `${articleText} 팔로우를 시도합니다`)
             try {
-              await this.workflowService.commentArticle(
-                page,
-                article as unknown as ElementHandle<HTMLDivElement>,
-                shuffle(filteredFollowMessages)[0],
-              )
-              this.logging(jobId, `${articleText} 댓글 성공`)
+              await this.workflowService.followArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
+              this.logging(jobId, `${articleText} 팔로우 성공`)
             } catch (err) {
-              this.logging(jobId, `${articleText} 댓글 결과: ${err.message}`)
+              this.logging(jobId, `${articleText} 팔로우 결과: ${err.message}`)
             }
+            await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
           }
-          await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
+          if (likeAction) {
+            this.logging(jobId, `${articleText} 좋아요를 시도합니다`)
+            try {
+              await this.workflowService.likeArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
+              this.logging(jobId, `${articleText} 좋아요 성공`)
+            } catch (err) {
+              this.logging(jobId, `${articleText} 좋아요 결과: ${err.message}`)
+            }
+            await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
+          }
+          if (repostAction) {
+            try {
+              this.logging(jobId, `${articleText} 리포스트를 시도합니다`)
+              await this.workflowService.repostArticle(page, article as unknown as ElementHandle<HTMLDivElement>)
+              this.logging(jobId, `${articleText} 리포스트 성공`)
+            } catch (err) {
+              this.logging(jobId, `${articleText} 리포스트 결과: ${err.message}`)
+            }
+            await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
+          }
+          if (commentAction) {
+            this.logging(jobId, `${articleText} 댓글을 시도합니다`)
+            const filteredFollowMessages = followMessages.filter(message => message.trim() !== '')
+            if (filteredFollowMessages.length === 0) {
+              this.logging(jobId, `멘트가 없어 댓글을 작성하지 않습니다.`)
+            } else {
+              try {
+                await this.workflowService.commentArticle(
+                  page,
+                  article as unknown as ElementHandle<HTMLDivElement>,
+                  shuffle(filteredFollowMessages)[0],
+                )
+                this.logging(jobId, `${articleText} 댓글 성공`)
+              } catch (err) {
+                this.logging(jobId, `${articleText} 댓글 결과: ${err.message}`)
+              }
+            }
+            await page.waitForTimeout(this.getRandomDelay(minDelay, maxDelay) * 1000)
+          }
+
+          processedArticles.add(articlaHash)
         }
       }
 
@@ -154,7 +158,10 @@ export class ThreadsService {
     await this.prisma.log.create({
       data: {
         jobId,
-        message,
+        // 4바이트(UTF-16 surrogate pair) 문자 제거
+        message: message
+          .replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g, '') // 4바이트 문자
+          .replace(/[\x00-\x1F\x7F]/g, ''), // 제어문자
       },
     })
   }
